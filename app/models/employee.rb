27e7174a8cdfaeaf8b2d4a4e1e384ab1782c2e6f
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-23 23:14:02 -0400

    Created appropriate association as well as index, show pages and ability to edit, and create a new employee plus validate certain fields

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+
+  belongs_to :dog
+
+  validates :alias, :title, presence: true, uniqueness: {case_sensitive: false, message: "is not unique. Try again partner!"}
+
 end
